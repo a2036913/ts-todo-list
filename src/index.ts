7d42232aa9737066e9f80726682c8227e436d2fe
a2036913 <a2036913@hotmail.com>
2022-09-22 23:42:07 +0800

    setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
-
-import confetti from 'canvas-confetti';
-
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+//to install this bundler ts setup
+//npx create-snowpack-app . --template @snowpack/app-template-blank-typescript --force
+import { v4 as uuidV4 } from 'uuid';
+console.log(uuidV4())
